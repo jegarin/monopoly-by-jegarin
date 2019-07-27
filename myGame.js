bb29aa42b553d1cfc195672587ad8532c3b2710c
Rodrigo Jikal <jikalrodrigo@gmail.com>
2019-07-27 17:52:41 -0300

    player1 moving and most of functions working

--- myGame.js
+++ myGame.js
@@ -1,28 +1,17 @@
-// alert('monopolyGame is connected');
 
-// const titleDeed = document.querySelectorAll('.title-deed');
-// const centralSquare = document.getElementById('sqr-central');
-
-// for(let i = 0; i < titleDeed.length; i += 1) {
-//   titleDeed[i].addEventListener('click', function() {
-//     console.log('title deed mouseenter');
-//   });
-
-// }
+const playerName = document.getElementById('player1-name');
+const playerCash = document.getElementById('player1-amount');
 
 const startBtn = document.getElementById('start-btn');
 startBtn.onclick = function() {
   player1.set();  
 }
 
-const playBtn = document.getElementById('play-btn');
-playBtn.onclick = function() {
+const rollBtn = document.getElementById('roll-btn');
+rollBtn.onclick = function() {
   player1.move();
   player1.displayInfo();
 }
-
-const playerName = document.getElementById('player1-name');
-const playerCash = document.getElementById('player1-amount');
 
 class Player {
   constructor(name, color) {
@@ -34,6 +23,7 @@ class Player {
 
   set() {
     playerName.textContent = this.name;
+    playerName.style.color = this.color;
     playerCash.textContent = this.cash;
     let oldSquare = 'sqr-' + this.position;
     let oldPosition = document.getElementById(oldSquare);
@@ -41,7 +31,6 @@ class Player {
     player1Square.setAttribute('id', 'player1-square');
     oldPosition.appendChild(player1Square);
     startBtn.style.display = 'none';
-
   }
 
   move() {
@@ -49,47 +38,111 @@ class Player {
     console.log(dice);
     const pos1 = this.position;
     this.position = (this.position + dice) % square.length;
-    this.updateCash();
-    this.updatePos(pos1, this.position);
+    
     this.printCardName();
 
-    // if communitychest
-    let cardGroup = square[this.position].group
-    if(cardGroup === 'group-communitychest') {
-      pickCommunityChest(player)
+    // analyze the current position, and take an action.
+    let cardGroup = square[this.position].group;
+    console.log(cardGroup)
+    switch (cardGroup) {
+      case 'group-communitychest':
+        pickCommunityChest(this);
+        break;
+
+      case 'group-chance':
+        pickChance(this);
+        break;
+
+      case 'group-tax':
+        if(this.position === 9) {
+          incomeTax(this);
+        } else if (this.position === 18) {
+          luxuryTax(this);
+        };
+        break;
+
+      case 'group-go-jail':
+        this.moveAction(-10);
+        break;
+
+      case 'group-go':
+        this.updateCash(200);
+        break;
+
+      case 'group-blue':
+        console.log('this is house is on the blue zone. You must pay $100.');
+        blueTitleDeed(this);
+        break;
+
+      case 'group-red':
+        console.log('this is house is on the red zone. You must pay $125.');
+        break;
+
+      case 'group-purple':
+        console.log('this is house is on the purple zone. You must pay $150.');
+        break;
+
+      case 'group-green':
+        console.log('this is house is on the green zone. You must pay $175.');
+        break;
+
+      default:
+        console.log('Essa square é uma propriedade sem função ainda');
     }
+
+    //invoke these fuctions after check  the square/card description and actions to be taken
+    this.updatePos(pos1, this.position);
+    this.checkIfPassedGo(pos1, this.position);
+
+    this.updatePlayer1Cash();
 
     if (this.cash < 0) {
       console.log('Game over for' + this.name);
     }
   };
 
-  updateCash(cash) {
-    this.cash += cash;
-    // this.cash += square[this.position].price;
+  updateCash(amount) {
+    console.log('the cash is: ' + this.cash + ' the amount is: ' + amount)
+    this.cash += amount;
+  }
 
+  moveAction(positions) {
+    console.log('the position is: '+ this.position + ' the amount is: ' + positions);
+    this.position += positions;
+  }
+
+  checkIfPassedGo(pos1, pos2) {
+    if(pos2 < pos1) {
+      this.updateCash(200);
+      console.log('passou pelo GO');
+    }
   }
 
   updatePos(pos1, pos2) {
-    // remove #player1-square at old position
-    let player1OldSquare = 'sqr-' + pos1;
-    let player1OldPosition = document.getElementById(player1OldSquare);
-    let child = player1OldPosition.querySelector('#player1-square');
-    player1OldPosition.removeChild(child);
+    // remove #player-square at old position
+    let playerOldSquare = 'sqr-' + pos1;
+    let playerOldPosition = document.getElementById(playerOldSquare);
+    let child = playerOldPosition.querySelector('#player1-square');
+    playerOldPosition.removeChild(child);
 
-    // update #player1-square at new position
-    let player1NewSquare = 'sqr-' + pos2;
-    let player1NewPosition = document.getElementById(player1NewSquare);
-    let player1Square = document.createElement('div');
-    player1Square.setAttribute('id', 'player1-square');
-    player1NewPosition.appendChild(player1Square);
+    // update #player-square at new position
+    let playerNewSquare = 'sqr-' + pos2;
+    let playerNewPosition = document.getElementById(playerNewSquare);
+    let playerSquare = document.createElement('div');
+    playerSquare.setAttribute('id', 'player1-square');
+    playerNewPosition.appendChild(playerSquare);
+  }
+
+  updatePlayer1Cash(){
+    const player1Amount = document.querySelector('#player1-amount');
+    player1Amount.innerHTML = this.cash;
   }
 
   printCardName() {
     let cardName = square[this.position].name;
     let displayCardName = document.getElementById('display-card-name');
     let p = document.createElement('p');
-    p.innerHTML = 'The card is a: ' + cardName;
+    p.innerHTML = 'You are at: ' + cardName;
     displayCardName.appendChild(p);
     setTimeout(function() {displayCardName.removeChild(p);}, 3000);
   }
@@ -97,107 +150,21 @@ class Player {
   displayInfo() {
     console.log(`${this.name} is at position ${ this.position} and has $ ${this.cash}`);
   }
-}
-
-
+  
+};
 
 const player1 = new Player('Rodrigo', 'red');
 const player2 = new Player('Jacinto', 'blue');
 const player3 = new Player('Waldisney', 'black');
-// turn 2
-/*
-// turn 1
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
 
 
-player1.move();
-player2.move();
-player3.move();
 
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
+// const titleDeed = document.querySelectorAll('.title-deed');
+// const centralSquare = document.getElementById('sqr-central');
 
-// turn 3
+// for(let i = 0; i < titleDeed.length; i += 1) {
+//   titleDeed[i].addEventListener('click', function() {
+//     console.log('title deed mouseenter');
+//   });
 
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-// turn 4
-
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-// turn 5
-
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-// turn 6
-
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-// turn 7
-
-player1.move();
-player2.move();
-player3.move();
-
-player1.displayInfo();
-player2.displayInfo();
-player3.displayInfo();
-
-console.log(1 % 20);
-console.log(2 % 20);
-console.log(3 % 20);
-console.log(4 % 20);
-console.log(5 % 20);
-console.log(6 % 20);
-console.log(7 % 20);
-console.log(8 % 20);
-console.log(9 % 20);
-console.log(10 % 20);
-console.log(11 % 20);
-console.log(12 % 20);
-console.log(13 % 20);
-console.log(14 % 20);
-console.log(15 % 20);
-console.log(16 % 20);
-console.log(17 % 20);
-console.log(18 % 20);
-console.log(19 % 20);
-console.log(20 % 20);
-console.log(21 % 20);
-console.log(22 % 20);
-console.log(23 % 20);
-console.log(40 % 20);
-console.log(45 % 20)
-;
-*/
+// }
--- squares.js
+++ squares.js
@@ -6,37 +6,39 @@ class Square {
   constructor (name, group) {
     this.name = name;
     this.group = group;
+    this.hasOwner = false;
+    this.playerOwner = '';
   }
 }
 
 let square = [];
 
 square[0] = new Square('Go', 'group-go');
-square[1] = new Square('Blue1', 'group-blue')
-square[2] = new Square('Community Chest1', 'group-communitychest')
-square[3] = new Square('Blue2', 'group-blue')
-square[4] = new Square('Chance1', 'group-chance')
-square[5] = new Square('In Jail', 'group-jail') 
-square[6] = new Square('Red1', 'group-red')
-square[7] = new Square('Community Chest2', 'group-communitychest')
-square[8] = new Square('Red2', 'group-red')
-square[9] = new Square('Income Tax', 'group-tax')
-square[10] = new Square('Free Parking', 'group-free')
-square[11] = new Square('Yellow1', 'group-yellow')
-square[12] = new Square('Community Chest3', 'group-communitychest')
-square[13] = new Square('Yellow2', 'group-yellow')
-square[14] = new Square('Yellow3', 'group-yellow')
-square[15] = new Square('Go to Jail', 'group-go-jail')
-square[16] = new Square('Chance2', 'group-chance')
-square[17] = new Square('Green1', 'group-green')
-square[18] = new Square('Luxury Tax', 'group-tax')
-square[19] = new Square('Yellow2', 'group-yellow')
+square[1] = new Square('Park Place', 'group-blue')
+square[2] = new Square('Community Chest, follow the instructions!', 'group-communitychest')
+square[3] = new Square('Broadwalk', 'group-blue')
+square[4] = new Square('Chance Opportunity. Pick a card and follow instruction', 'group-chance')
+square[5] = new Square('Jail, but just visiting!', 'group-jail') 
+square[6] = new Square('Illinois Avenue', 'group-red')
+square[7] = new Square('Community Chest, follow the instructions!', 'group-communitychest')
+square[8] = new Square('Indiana Avenue', 'group-red')
+square[9] = new Square('Income Tax Day! Pays $200 to the Bank', 'group-tax')
+square[10] = new Square('Free Parking. You won\'t receive cash, but no need to pay!', 'group-free')
+square[11] = new Square('Marvin Gardens', 'group-purple')
+square[12] = new Square('Community Chest, follow the instructions!', 'group-communitychest')
+square[13] = new Square('Ventnor Avenue', 'group-purple')
+square[14] = new Square('Atlantic Avenue', 'group-purple')
+square[15] = new Square('Go visit the Jail. Good Luck!', 'group-go-jail')
+square[16] = new Square('Chance Opportunity. Pick a card and follow instruction', 'group-chance')
+square[17] = new Square('Pacific Avenue', 'group-green')
+square[18] = new Square('Luxury Tax Day! Pay $75 to the Bank', 'group-tax')
+square[19] = new Square('Pennsylvania Avenue', 'group-green')
 // square[20] = new Square('GO2', 200)
 
 class Card {
-  constructor(name, cash){
+  constructor(name, amount){
     this.name = name;
-    this.cash = cash;
+    this.amount = amount;
   }
 }
 
@@ -44,8 +46,8 @@ let chanceCards = [];
 
 chanceCards[0] = new Card ('Speeding fine $15.', -15);
 chanceCards[1] = new Card ('Bank pays you dividend of $50.', 50);
-chanceCards[2] = new Card ('Pay poor tax of $15.', 15);
-chanceCards[3] = new Card ('Go back three spaces.', 3);
+chanceCards[2] = new Card ('Pay poor tax of $15.', -15);
+chanceCards[3] = new Card ('Go back three spaces.', -3);
 chanceCards[4] = new Card ('Advance to GO and collect $200.', 200);
 
 let communityChestCards = []
@@ -63,20 +65,69 @@ communityChestCards[9] = new Card ('Doctor\'s fee. Pay $50.', -50);
 
 function pickCommunityChest(player) {
   let total = communityChestCards.length;
-  let randomNumber = Math.ceil(Math.random() * total);
-  return communityChestCards[randomNumber];
-  player.updateCash(communityChestCards[randomNumber].cash)
-
+  let randomNumber = Math.floor(Math.random() * total);
+  let randomCard = communityChestCards[randomNumber];
+  console.log(randomCard);
+  console.log('pickCommunityChest function invoked, check problem with amount in case is true')
+  let result = player.updateCash(randomCard.amount);
+  return result;
 }
 
-// function addAmount(amount){
-//   return this.cash += amount;
-// }
-
-function luxuryTax() {
-
+function pickChance(player) {
+  let total = chanceCards.length;
+  let randomNumber = Math.floor(Math.random() * total);
+  // let randomNumber = 3;
+  let randomCard = chanceCards[randomNumber];
+  console.log(randomCard);
+  if(randomNumber === 3) {
+    let result1 = player.moveAction(randomCard.amount);
+    console.log('this is result1: ' + result1)
+    return result1; //  insert a function to check the new current possition description and action;
+  } else {
+    let result2 = player.updateCash(randomCard.amount);
+    console.log('this is result2: ' + result2)
+    return result2;
+  }
 }
 
-function incomeTax() {
-
+function luxuryTax(player) {
+  let luxuryTaxCash = -75;
+  player.updateCash(luxuryTaxCash);
 }
+
+function incomeTax(player) {
+  let incomeTaxCash = -200;
+  player.updateCash(incomeTaxCash);
+}
+
+function blueTitleDeed(player){
+  // buy $1350 - rent $50
+  // check if has ownwer ? pay rent : buy the propertie;
+  if(player.position === 3){
+    console.log('Here is Park Place');
+    console.log(square[player.position].hasOwner)
+    if(!square[player.position].hasOwner) {
+      // buy the prop and mark as owner
+      square[player.position].playerOwner = player.name;
+      square[player.position].hasOwner = true; 
+      player.updateCash(-1350);
+    } 
+    else if(!square[player.position].playerOwner === player.name)  {
+      //pay rent
+      player.updateCash(-50);
+    }
+  }
+  else {
+    console.log('Here is Broadwalk');
+    console.log(square[player.position].hasOwner)
+    if(!square[player.position].hasOwner) {
+      // buy the prop
+    square[player.position].hasOwner = true
+    player.updateCash(-1750);
+    }
+    else if (!square[player.position].playerOwner === player.name) {
+    //  pay rent
+    player.updateCash(-75);
+    }
+  }
+};
